5842026cbdd2120f963b9c98f1ce0ba11e62f236
Corentin Barreau <corentin.barreau@epitech.eu>
2018-08-08 22:48:19 +0200

    Add: everything, rewrote the entire tool to use suggestion for grabbing youtube IDs.

--- youtube-ig.go
+++ youtube-ig.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"os"
 	"runtime"
-	"strings"
+	"strconv"
 	"sync"
 	"time"
 
@@ -19,67 +19,66 @@ import (
 
 var checkPre = color.Yellow("[") + color.Green("✓") + color.Yellow("]") + color.Yellow("[")
 
-func processWordResult(word string, page int) int {
-	res, err := http.Get("https://www.youtube.com/results?search_query=" + word + "&gl=US&hl=en&disable_polymer=true&page=" + string(page))
-	if err != nil {
-		color.Println(color.Yellow("[") + color.Red("!") + color.Yellow("]") + color.Yellow("[") + color.Cyan(word) + color.Yellow("]") + color.Red(" No result for word: ") + color.Yellow(word))
-		runtime.Goexit()
-		return 84
-	}
-	// defer it!
-	defer res.Body.Close()
-	// check status, exit if != 200
-	if res.StatusCode != 200 {
-		color.Println(color.Yellow("[") + color.Red("!") + color.Yellow("]") + color.Yellow("[") + color.Cyan(word) + color.Yellow("]") + color.Red(" No more result for word: ") + color.Yellow(word))
-		runtime.Goexit()
-		return 84
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	// start goquery in the page
-	document, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		runtime.Goexit()
-		return 84
-	}
-	// open file
-	file, err := os.OpenFile("ids.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-	// get IDs
-	document.Find("a").Each(func(i int, s *goquery.Selection) {
-		if name, _ := s.Attr("dir"); name == "ltr" {
-			ID, _ := s.Attr("href")
-			if strings.Contains(ID, "/watch?v=") == true && len(ID) == 20 {
-				color.Println(checkPre + color.Cyan(word) + color.Yellow("] ") + color.Green("ID found: ") + color.Yellow(ID[9:]))
-				if _, err = file.WriteString(ID[9:] + "\n"); err != nil {
-					panic(err)
-				}
-			}
-		}
-	})
-	return 0
-}
-
-func processWord(word string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	var page = 1
-	for i := 1; i != 84; page++ {
-		time.Sleep(1 * time.Second)
-		if processWordResult(word, page) == 84 {
-			i = 84
+func appendID(path string, ID string, file *os.File) {
+	// scan the list line by line
+	scanner := bufio.NewScanner(file)
+	// scan the list line by line
+	for scanner.Scan() {
+		if scanner.Text() == ID {
 			runtime.Goexit()
 		}
 	}
+	// log if error
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := f.Write([]byte(ID + "\n")); err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(checkPre + color.Cyan(ID) + color.Yellow("]") + color.Green(" Added to the list!"))
 }
 
-func readList(path string) {
+func grabSuggest(path string, ID string, file *os.File) {
+	// request video html page
+	html, err := http.Get("http://youtube.com/watch?v=" + ID + "&gl=US&hl=en&has_verified=1&bpctr=9999999999")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		runtime.Goexit()
+	}
+	// check status, exit if != 200
+	if html.StatusCode != 200 {
+		fmt.Fprintf(os.Stderr, "Status code error for %s: %d %s", ID, html.StatusCode, html.Status)
+		runtime.Goexit()
+	}
+	body, err := ioutil.ReadAll(html.Body)
+	// start goquery in the page
+	document, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
+	// extract suggested IDs
+	document.Find("span").Each(func(i int, s *goquery.Selection) {
+		if name, _ := s.Attr("class"); name == "yt-uix-simple-thumb-wrap yt-uix-simple-thumb-related" {
+			videoID, _ := s.Attr("data-vid")
+			appendID(path, videoID, file)
+		}
+	})
+}
+
+func processSingleID(path string, ID string, worker *sync.WaitGroup, file *os.File) {
+	defer worker.Done()
+	grabSuggest(path, ID, file)
+}
+
+func processList(maxConc int64, path string, worker *sync.WaitGroup) {
+	defer worker.Done()
+	var count int64
 	// start workers group
 	var wg sync.WaitGroup
-	var count int
 	// open file
 	file, err := os.Open(path)
 	if err != nil {
@@ -92,8 +91,8 @@ func readList(path string) {
 	for scanner.Scan() {
 		count++
 		wg.Add(1)
-		go processWord(scanner.Text(), &wg)
-		if count == 16 {
+		go processSingleID(path, scanner.Text(), &wg, file)
+		if count == maxConc {
 			wg.Wait()
 			count = 0
 		}
@@ -103,19 +102,32 @@ func readList(path string) {
 		log.Fatal(err)
 	}
 	wg.Wait()
+	processList(maxConc, path, worker)
 }
 
-func startGrabbing(args []string) {
-	if _, err := os.Stat(args[0]); err == nil {
-		readList(args[0])
-	} else {
-		fmt.Println("You need to provide a list as argument.")
+func argumentParsing(args []string) {
+	// start workers group
+	var wg sync.WaitGroup
+	var maxConc int64
+	maxConc = 16
+	wg.Add(1)
+	if len(args) > 2 {
+		color.Red("Usage: ./youtube-ig [list of IDs] [CONCURRENCY]")
 		os.Exit(1)
+	} else if len(args) == 2 {
+		if _, err := strconv.ParseInt(args[1], 10, 64); err == nil {
+			maxConc, _ = strconv.ParseInt(args[1], 10, 64)
+		} else {
+			color.Red("Usage: ./youtube-ig [list of IDs] [CONCURRENCY]")
+			os.Exit(1)
+		}
 	}
+	go processList(maxConc, args[0], &wg)
+	wg.Wait()
 }
 
 func main() {
 	start := time.Now()
-	startGrabbing(os.Args[1:])
+	argumentParsing(os.Args[1:])
 	color.Println(color.Cyan("Done in ") + color.Yellow(time.Since(start)) + color.Cyan("!"))
 }
